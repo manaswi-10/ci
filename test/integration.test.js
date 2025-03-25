909948338900a8ecb7e31414cc07ee003bb3ca2a
manaswi-10 <143397498+manaswi-10@users.noreply.github.com>
2025-03-25 15:57:20 +0530

    Update integration.test.js

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,17 +1,17 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const expect = chai.expect;
 
 chai.use(chaiHttp);
+const { expect } = chai;
 
-describe('Integration Test: Nginx Web Server', () => {
-  it('should return status 200 and the greeting from the HTML file', (done) => {
-    chai.request('http://localhost:8080&#39;)  // Assuming Docker container is mapped to port 8080
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);  // Ensure the server returns 200 status code
-        expect(res.text).to.include('Hello from Docker!');  // Ensure the HTML content is correct
-        done();
-      });
-  });
+describe('Integration Test', function () {
+    it('should return a 200 status for the homepage', function (done) {
+        chai.request('http://localhost:8080') // Make sure this URL is properly formatted
+            .get('/')
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
 });
